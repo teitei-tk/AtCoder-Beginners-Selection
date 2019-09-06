464d65601c387c8e145467615efb13d05d6516e2
teitei-tk <teitei.tk@gmail.com>
2019-09-06 18:44:09 +0900

    add PracticeA task code

--- src/PracticeA/main.py
+++ src/PracticeA/main.py
@@ -1,6 +1,10 @@
-def main(r):
-  pass
+def main() -> None:
+  a = int(input())
+  b = sum([int(x) for x in str(input()).strip().split(" ")])
+  c = str(input())
+
+  result = "{0} {1}".format(int(a + b), c)
+  print(result)
 
 if __name__ == "__main__":
-  r = str(input())
-  main(r)
+  main()

3aeef15cffcae9019fee97bb8f2a526fe1d9f33f
teitei-tk <teitei.tk@gmail.com>
2019-09-07 23:13:22 +0900

    add ABC081A abs

---
+++ src/ABC081A/main.py
@@ -0,0 +1,16 @@
+def main():
+    count = 0
+    for w in input():
+        if w == '1':
+            count += 1
+
+    print(count)
+
+# or
+#
+# def main():
+#     str(input()).count('1')
+
+
+if __name__ == "__main__":
+    main()
